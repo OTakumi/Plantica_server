f4f60f5fe2eb8abb31b442516b61ae1a9c55bdb6
OTakumi <takumi.o.0410@gmail.com>
2025-04-20 22:16:28 +0900

    Add UserLoginDto class for user authentication
    Introduced a new `UserLoginDto` class in the
    `Plantica.Application.DTOs` namespace. This class
    includes two required properties: `Username` and
    `Password`, both of type `string`, to facilitate
    user login functionality.

---
+++ Application/Plantica.Application.DTO/UserLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Plantica.Application.DTOs
+{
+    public class UserLoginDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
